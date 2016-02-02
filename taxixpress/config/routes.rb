5c7123286fb748bce5627b14460ce15214e841e0
Rhony Arevalo <rhonyarevalo@gmail.com>
2016-02-02 00:29:20 +0000

    construcion front-end

--- taxixpress/config/routes.rb
+++ taxixpress/config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about_us'
+
+  get 'pages/faq'
+
+  get 'pages/tos'
+
+  get 'pages/contact_us'
+  
+  get 'creatives/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
