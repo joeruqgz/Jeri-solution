254b2f66bc1c5e2bb4923c4eeeabae6a831d1c64
Rhony Arevalo <rhonyarevalo@gmail.com>
2016-02-23 00:48:57 +0000

    permiso de admin, form para tarifa

--- taxixpress/db/migrate/20160216014359_create_assignments.rb
+++ taxixpress/db/migrate/20160216014359_create_assignments.rb
@@ -2,7 +2,7 @@ class CreateAssignments < ActiveRecord::Migration
   def change
     create_table :assignments do |t|
       t.integer :id_assignment
-      t.references :taxis, index: true, foreign_key: true
+      t.references :taxi, index: true, foreign_key: true
       t.integer :id_travel
       t.integer :status
       t.integer :id_user_operator
